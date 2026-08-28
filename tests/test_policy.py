bea6335016774a4b31d52800c49259459329c96a
Patrick Luzolo <eldorplus@gmail.com>
2026-08-28 12:05:30 +0200

    test: add policy filtering test

---
+++ tests/test_policy.py
@@ -0,0 +1,11 @@
+from app.services.policy_service import PolicyService
+from app.services.webhook_service import ManualJob
+
+
+def job():
+    return ManualJob(42, "demo", "", 1, 2, "deploy_production", "deploy", "manual", "main", "abc", "", "")
+
+
+def test_empty_policy_allows():
+    service = PolicyService("does-not-exist.yml")
+    assert service.allowed(job()) is True
